934d89eb9ae25590685154f8ba62bd2242e8b06c
Yousseftame <tamerosama73@gmail.com>
2026-01-13 14:38:36 +0200

    temp rename navbar & footer

--- src/layouts/MasterLayOut/MasterLayOut.tsx
+++ src/layouts/MasterLayOut/MasterLayOut.tsx
@@ -1,7 +1,7 @@
 
 import { Outlet } from 'react-router-dom';
-import Navbar from '../../components/navbar/Navbar';
-import Footer from '../../components/footer/Footer';
+import Navbar from '../../components/navbar-temp/Navbar';
+import Footer from '../../components/footer-temp/Footer';
 import ScrollToTop from '../../components/shared/ScrollToTop';
 
 
