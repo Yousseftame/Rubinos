ad50aed16fd2d22baaae01dd0ad9752cc4f05769
Yousseftame <tamerosama73@gmail.com>
2026-01-13 13:46:20 +0200

    menu details 3

--- src/layouts/MasterLayOut/MasterLayOut.tsx
+++ src/layouts/MasterLayOut/MasterLayOut.tsx
@@ -1,14 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import ScrollToTop from '../../components/shared/ScrollToTop';
-import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
-
+import Navbar from '../../components/navbar/Navbar';
 
 const MasterLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-background">
       <ScrollToTop />
-      <Navbar />
+       <Navbar />
       <main className="flex-1">
         <Outlet/>
       </main>
